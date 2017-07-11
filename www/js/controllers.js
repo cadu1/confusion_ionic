fbd4d4d8a99535cdd224a7b3f30c3a8d70ae3f38
Carlos Oliveira <carlos.eduardo02@gmail.com>
2017-07-10 22:38:27 -0300

    Refactoring some codes!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -77,7 +77,7 @@ angular.module('conFusion.controllers', [])
       $ionicListDelegate.closeOptionButtons();
     }
 
-    menuFactory.getDishes().query(
+    menuFactory.query(
       function (response) {
         $scope.dishes = response;
         $scope.showMenu = true;
@@ -109,32 +109,14 @@ angular.module('conFusion.controllers', [])
     };
   }])
 
-  .controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$ionicLoading', '$timeout', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $ionicLoading, $timeout) {
+  .controller('FavoritesController', ['$scope', 'dishes', 'favorites', 'favoriteFactory', 'baseURL', '$ionicListDelegate', '$ionicPopup', '$ionicLoading', '$timeout', function ($scope, dishes, favorites, favoriteFactory, baseURL, $ionicListDelegate, $ionicPopup, $ionicLoading, $timeout) {
 
     $scope.baseURL = baseURL;
     $scope.shouldShowDelete = false;
 
-    $ionicLoading.show({
-      template: '<ion-spinner></ion-spinner> Loading...'
-    });
+    $scope.favorites = favorites;
 
-    $scope.favorites = favoriteFactory.getFavorites();
-
-    $scope.dishes = menuFactory.getDishes().query(
-      function (response) {
-        $scope.dishes = response;
-        $timeout(function () {
-          $ionicLoading.hide();
-        }, 1000);
-      },
-      function (response) {
-        $scope.message = "Error: " + response.status + " " + response.statusText;
-        $timeout(function () {
-          $ionicLoading.hide();
-        }, 1000);
-      }
-    );
-    console.log($scope.dishes, $scope.favorites);
+    $scope.dishes = dishes;
 
     $scope.toggleDelete = function () {
       $scope.shouldShowDelete = !$scope.shouldShowDelete;
@@ -189,28 +171,20 @@ angular.module('conFusion.controllers', [])
     };
   }])
 
-  .controller('DishDetailController', ['$scope', '$stateParams', 'favoriteFactory', 'menuFactory', 'baseURL', '$ionicPopover', '$ionicModal', function ($scope, $stateParams, favoriteFactory, menuFactory, baseURL, $ionicPopover, $ionicModal) {
+  .controller('DishDetailController', ['$scope', '$stateParams', 'dish', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicPopover', '$ionicModal', function ($scope, $stateParams, dish, menuFactory, favoriteFactory, baseURL, $ionicPopover, $ionicModal) {
 
     $scope.baseURL = baseURL;
-    $scope.showDish = false;
+
+    $scope.dish = dish;
     $scope.message = "Loading ...";
+    $scope.showDish = false;
     $scope.mycomment = { rating: 5, comment: "", author: "", date: "" };
     $scope.id = parseInt($stateParams.id, 10);
-
-    $scope.dish = menuFactory.getDishes().get({ id: parseInt($stateParams.id, 10) })
-      .$promise.then(
-      function (response) {
-        $scope.dish = response;
-        $scope.showDish = true;
-      },
-      function (response) {
-        $scope.message = "Error: " + response.status + " " + response.statusText;
-      });
 
     $scope.submitComment = function () {
       $scope.mycomment.date = new Date().toISOString();
       $scope.dish.comments.push($scope.mycomment);
-      menuFactory.getDishes().update({ id: $scope.dish.id }, $scope.dish);
+      menuFactory.update({ id: $scope.dish.id }, $scope.dish);
       $scope.popover.hide();
       $scope.modal.hide();
     };
@@ -260,7 +234,7 @@ angular.module('conFusion.controllers', [])
       console.log($scope.mycomment);
 
       $scope.dish.comments.push($scope.mycomment);
-      menuFactory.getDishes().update({ id: $scope.dish.id }, $scope.dish);
+      menuFactory.update({ id: $scope.dish.id }, $scope.dish);
 
       $scope.commentForm.$setPristine();
 
@@ -268,24 +242,28 @@ angular.module('conFusion.controllers', [])
     }
   }])
 
-  // implement the IndexController and About Controller here
-  .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', 'baseURL', function ($scope, menuFactory, corporateFactory, baseURL) {
+  .controller('IndexController', ['$scope', 'menuFactory', 'promotionFactory', 'corporateFactory', 'baseURL', function ($scope, menuFactory, promotionFactory, corporateFactory, baseURL) {
 
     $scope.baseURL = baseURL;
-    $scope.leader = corporateFactory.get({ id: 3 });
+    $scope.leader = corporateFactory.get({
+      id: 3
+    });
+
     $scope.showDish = false;
     $scope.message = "Loading ...";
-    $scope.dish = menuFactory.getDishes().get({ id: 0 })
-      .$promise.then(
+
+    $scope.dish = menuFactory.get({ id: 0 }).$promise.then(
       function (response) {
         $scope.dish = response;
         $scope.showDish = true;
       },
       function (response) {
         $scope.message = "Error: " + response.status + " " + response.statusText;
-      }
-      );
-    $scope.promotion = menuFactory.getPromotion().get({ id: 0 });
+      });
+
+    $scope.promotion = promotionFactory.get({
+      id: 0
+    });
 
   }])
 
--- www/js/services.js
+++ www/js/services.js
@@ -3,17 +3,17 @@
 angular.module('conFusion.services', ['ngResource'])
     .constant("baseURL", "http://localhost:3000/")
 
-    .service('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
-        this.getDishes = function () {
-            return $resource(baseURL + "dishes/:id", null, { 'update': { method: 'PUT' } });
-        };
+        return $resource(baseURL + "dishes/:id", null, {
+            'update': {
+                method: 'PUT'
+            }
+        });
+    }])
 
-        // implement a function named getPromotion
-        // that returns a selected promotion.
-        this.getPromotion = function () {
-            return $resource(baseURL + "promotions/:id");;
-        }
+    .factory('promotionFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+        return $resource(baseURL + "promotions/:id");
     }])
 
     .factory('corporateFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
